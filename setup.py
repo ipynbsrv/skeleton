e24ace1cf4c850c5e887d79111703df048be63b0
Michel Käser <mail@michelkaeser.ch>
2015-10-07 21:14:04 +0200

    name changes for new project name (coco); added BSD 3-clause license$

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import os
 from setuptools import setup, find_packages
 
 setup(
-    name="ipynbsrv-skeleton",
-    description="Project skeleton for ipynbsrv Python packages.",
+    name="coco-skeleton",
+    description="Project skeleton for coco Python packages.",
     version="0.0.1",
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    namespace_packages=['ipynbsrv'],
+    namespace_packages=['coco'],
     install_requires=[],
 )
